6e9e9ced9e699f950e26298f69ce5523b37c0540
iamhabee <habeehola18@gmail.com>
2023-06-01 10:54:52 +0100

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+import dns from 'dns'
+
+dns.setDefaultResultOrder('verbatim')
+
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
